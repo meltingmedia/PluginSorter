176a113546a2e5cba2c7cd059f4ae78bfbc0621e
Romain <romain@melting-media.com>
2013-04-22 20:49:21 +0200

    Prevent drop when over a group header

--- manager/components/pluginsorter/js/home/plugins.grid.js
+++ manager/components/pluginsorter/js/home/plugins.grid.js
@@ -109,18 +109,25 @@ PluginSorter.PluginsGrid = function(config) {
                 ,fn: function(grid) {
                     this.dropTarget = new Ext.dd.DropTarget(grid.getView().mainBody, {
                         ddGroup: 'plugins'
+                        ,grid: this
                         ,copy: false
                         ,notifyOver: function(dragSource, e, data) {
                             if (dragSource.getDragData(e)) {
                                 var targetNode = dragSource.getDragData(e).selections[0]
-                                    ,sourceNode = data.selections[0];
+                                    ,sourceNode = data.selections[0]
+                                    ,t = Ext.lib.Event.getTarget(e)
+                                    ,rindex = this.grid.getView().findRowIndex(t);
 
-                                if (sourceNode.data.event != targetNode.data.event) {
+                                if (rindex == data.rowIndex) rindex = false;
+
+                                if ((sourceNode.data.event != targetNode.data.event) || rindex === false || rindex < 0) {
                                     return this.dropNotAllowed;
                                 }
+
+                                return this.dropAllowed;
                             }
 
-                            return this.dropAllowed;
+                            return this.dropNotAllowed;
                         }
                         ,notifyDrop: function(dragSource, e, data) {
                             var sm = grid.getSelectionModel()
